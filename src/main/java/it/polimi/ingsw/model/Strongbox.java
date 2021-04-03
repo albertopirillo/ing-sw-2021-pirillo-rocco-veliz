f80c2d077b21f74b4aa6271ad5d7e9d54f8c41c2
AlbertoPirillo <alberto.pirillo@mail.polimi.it>
2021-04-03 15:36:11 +0200

    Added Strongbox class code and test cases

--- src/main/java/it/polimi/ingsw/model/Strongbox.java
+++ src/main/java/it/polimi/ingsw/model/Strongbox.java
@@ -1,26 +1,49 @@
 package it.polimi.ingsw.model;
 
+import it.polimi.ingsw.exceptions.*;
+
 import java.util.*;
 
+//Cannot contain Faith
 public class Strongbox {
 
+    private final Resource resource;
+
     public Strongbox() {
+        this.resource = new Resource(0,0,0,0);
     }
 
-    private Resource resource;
-
-
-    public Resource getResource() {
-        // TODO implement here
-        return null;
+    //Returns a copy of all the resources stored
+    public Map<ResourceType, Integer> queryAllRes() {
+        return new HashMap<>(resource.getAllRes());
     }
 
-    public void addResource(Resource resource) {
-        // TODO implement here
+    public void addResources(Resource resource) throws CannotContainFaithException, NegativeResAmountException, InvalidKeyException {
+        Map<ResourceType, Integer> copy = resource.getAllRes();
+        if ((copy.get(ResourceType.FAITH) != null)) throw new CannotContainFaithException();
+
+        for (ResourceType key: copy.keySet()) {
+            this.resource.modifyValue(key, copy.get(key));
+        }
     }
 
-    public void moveToDepot(Resource resource) {
-        // TODO implement here
+    public void retrieveResources(Resource resource) throws CannotContainFaithException, NotEnoughResException, NegativeResAmountException, InvalidKeyException {
+        Map<ResourceType, Integer> toTake = resource.getAllRes();
+        if ((toTake.get(ResourceType.FAITH) != null)) throw new CannotContainFaithException();
+
+        if (!this.resource.compare(resource)) throw new NotEnoughResException();
+
+        //removes the requested resources from the strongbox
+        for (ResourceType key: toTake.keySet()) {
+            this.resource.modifyValue(key, - toTake.get(key));
+        }
+    }
+
+    //3x resources can be moved from Strongbox to Depot, at the cost of 1x resource
+    public void moveToDepot(ResourceType resType, Depot depot, int layer) throws NegativeResAmountException, InvalidKeyException, CannotContainFaithException, InvalidLayerNumberException, LayerNotEmptyException, NotEnoughSpaceException, NotEnoughResException, AlreadyInAnotherLayerException {
+        if (this.resource.getValue(resType) < 3) throw new NotEnoughResException();
+        depot.modifyLayer(layer, resType, 2);
+        this.resource.modifyValue(resType, -3);
     }
 
 }
---
+++ src/test/java/it/polimi/ingsw/model/StrongboxTest.java
@@ -0,0 +1,69 @@
+package it.polimi.ingsw.model;
+
+import it.polimi.ingsw.exceptions.*;
+import org.junit.jupiter.api.Test;
+
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StrongboxTest {
+
+    @Test
+    public void queryAllRes() {
+        Strongbox strongbox = new Strongbox();
+        Map<ResourceType, Integer> res = strongbox.queryAllRes();
+        assertEquals(4, res.size());
+        for(ResourceType key: res.keySet()) {
+            assertEquals(0, res.get(key));
+        }
+    }
+
+    @Test
+    public void addResources() throws CannotContainFaithException, NegativeResAmountException, InvalidKeyException {
+        Strongbox strongbox = new Strongbox();
+        Resource res = new Resource(2, 3, 4, 5);
+        strongbox.addResources(res);
+        Map<ResourceType, Integer> content = strongbox.queryAllRes();
+        for (ResourceType key: content.keySet()) {
+            assertEquals(res.getValue(key), content.get(key));
+        }
+        Resource res2 = new Resource();
+        res2.addResource(ResourceType.FAITH, 3);
+        assertThrows(CannotContainFaithException.class, () -> strongbox.addResources(res2));
+    }
+
+    @Test
+    public void retrieveResources() throws CannotContainFaithException, NegativeResAmountException, InvalidKeyException, NotEnoughResException {
+        Strongbox strongbox = new Strongbox();
+        strongbox.addResources(new Resource(2, 3, 4, 0));
+        Resource resOK = new Resource(2, 1, 3, 0);
+        Resource resKO = new Resource(2, 1, 5, 0);
+        assertThrows(NotEnoughResException.class, () -> strongbox.retrieveResources(resKO));
+
+        strongbox.retrieveResources(resOK);
+        Map<ResourceType, Integer> content = strongbox.queryAllRes();
+        Resource checkRes = new Resource(0, 2, 1, 0);
+        for (ResourceType key: content.keySet()) {
+            assertEquals(checkRes.getValue(key), content.get(key));
+        }
+    }
+
+    @Test
+    public void moveToDepot() throws CannotContainFaithException, NegativeResAmountException, InvalidKeyException, NotEnoughSpaceException, InvalidLayerNumberException, LayerNotEmptyException, NotEnoughResException, AlreadyInAnotherLayerException {
+        Strongbox strongbox = new Strongbox();
+        strongbox.addResources(new Resource(2, 3, 4, 0));
+        Strongbox strongbox2 = new Strongbox();
+        strongbox2.addResources(new Resource(2, 3, 2, 0));
+        Depot depot = new ConcreteDepot();
+        depot.modifyLayer(3, ResourceType.SERVANT, 1);
+
+        assertThrows(NotEnoughResException.class, () -> strongbox2.moveToDepot(ResourceType.SHIELD, depot, 2));
+        assertThrows(LayerNotEmptyException.class, () -> strongbox.moveToDepot(ResourceType.SHIELD, depot, 3));
+        strongbox.moveToDepot(ResourceType.SHIELD, depot, 2);
+        assertEquals(2, depot.getLayer(2).getAmount());
+        assertEquals(ResourceType.SHIELD, depot.getLayer(2).getResource());
+        Map<ResourceType, Integer> content = strongbox.queryAllRes();
+        assertEquals(1, content.get(ResourceType.SHIELD));
+    }
+}

70c555f6626bfab41d2bfcf97182087c5d2a7b89
aldairveliz <aldair99veliz@gmail.com>
2021-04-06 11:43:16 +0200

    Update init Market

--- src/test/java/it/polimi/ingsw/model/MarketTest.java
+++ src/test/java/it/polimi/ingsw/model/MarketTest.java
@@ -1,8 +1,14 @@
 package it.polimi.ingsw.model;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import com.google.gson.stream.JsonReader;
 import it.polimi.ingsw.exceptions.FullCardDeckException;
 import org.junit.jupiter.api.Test;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.lang.reflect.Type;
 import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -10,6 +16,33 @@ import static org.junit.jupiter.api.Assertions.*;
 class MarketTest {
 
     @Test
+    public void parserTest() throws FullCardDeckException {
+
+        Market market = new Market();
+        List<DevelopmentCard> devs = market.getAvailableCards();
+        //remove null elements to test the deck empty, is optional
+        //put 11 cards(of 12 cards) so one slot is empty -> so an element of list is null
+        devs.removeIf(Objects::isNull);
+        DevelopmentCard dev1 = new DevelopmentCard(new Resource(4,2,1,2), CardColor.GREEN, 1);
+        DevelopmentCard dev2 = new DevelopmentCard(new Resource(1,1,3,2), CardColor.BLUE, 1);
+        DevelopmentCard dev3 = new DevelopmentCard(new Resource(3,1,3,4), CardColor.YELLOW, 1);
+        DevelopmentCard dev4 = new DevelopmentCard(new Resource(1,2,4,2), CardColor.PURPLE, 1);
+        assertEquals(devs.size(), 11);
+        assertEquals(devs.get(7), dev1);
+        assertEquals(devs.get(8), dev2);
+        assertEquals(devs.get(9), dev3);
+        assertEquals(devs.get(10), dev4);
+
+        List<DevelopmentCard> devCards = new ArrayList<>();
+        devCards.add(new DevelopmentCard(new Resource(1,1,3,2), CardColor.GREEN, 3));
+        devCards.add(new DevelopmentCard(new Resource(4,4,2,1), CardColor.BLUE, 3));
+        devCards.add(new DevelopmentCard(new Resource(1,2,4,1), CardColor.YELLOW, 3));
+        String json = new Gson().toJson(devCards);
+
+
+
+    }
+    /*@Test
     public void getAvailableCards() throws FullCardDeckException {
         List<DevelopmentCard> devCards = new ArrayList<>();
         devCards.add(new DevelopmentCard(new Resource(1,1,3,2), CardColor.GREEN, 3));
@@ -42,7 +75,7 @@ class MarketTest {
         assertEquals(devs.get(8), devCards.get(8));
         assertEquals(devs.get(9), devCards.get(9));
         assertEquals(devs.get(10), devCards.get(10));
-    }
+    }*/
 
     @Test
     public void buyCards() {

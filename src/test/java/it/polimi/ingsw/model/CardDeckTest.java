a3b0f19beaaa58b9ec84617f97eb1f3fe7a19e0c
aldairveliz <aldair99veliz@gmail.com>
2021-04-08 02:11:24 +0200

    Update Market init and added test case

--- src/test/java/it/polimi/ingsw/model/CardDeckTest.java
+++ src/test/java/it/polimi/ingsw/model/CardDeckTest.java
@@ -34,15 +34,20 @@ class CardDeckTest {
         DevelopmentCard dev1 = new DevelopmentCard(1, res1, CardColor.BLUE, 2, new ProductionPower(res1, res2));
         DevelopmentCard dev2 = new DevelopmentCard(2, res2, CardColor.BLUE, 2, new ProductionPower(res1, res2));
         DevelopmentCard dev3 = new DevelopmentCard(3, res3, CardColor.BLUE, 2, new ProductionPower(res1, res2));
+        DevelopmentCard dev4 = new DevelopmentCard(3, res1, CardColor.BLUE, 2, new ProductionPower(res1, res2));
         deck.addCard(dev1);
         deck.addCard(dev2);
         deck.addCard(dev3);
-        assertEquals(deck.getNumbersOfCards(), 3);
+        deck.addCard(dev4);
+        assertEquals(deck.getNumbersOfCards(), 4);
         DevelopmentCard devCard = deck.removeCard();
-        assertEquals(deck.getNumbersOfCards(), 2);
+        assertEquals(deck.getNumbersOfCards(), 3);
         DevelopmentCard devCard1 = deck.removeCard();
+        assertEquals(deck.getNumbersOfCards(), 2);
+        DevelopmentCard devCard2 = deck.removeCard();
         assertEquals(deck.getNumbersOfCards(), 1);
-        boolean bool = devCard.equals(dev2) || devCard.equals(dev3) || devCard1.equals(dev3);
+        boolean bool = devCard.equals(dev2) || devCard.equals(dev3) || devCard.equals(dev4) ||
+                devCard1.equals(dev3) || devCard1.equals(dev4) || devCard2.equals(dev4) ;
         assertTrue(bool);
     }
 
@@ -80,12 +85,23 @@ class CardDeckTest {
         Resource res1 = new Resource(1,2,3,4);
         Resource res2 = new Resource(4,3,2,1);
         Resource res3 = new Resource(2,4,1,3);
+        Resource res4 = new Resource(1,4,1,1);
         DevelopmentCard dev1 = new DevelopmentCard(2, res1, CardColor.BLUE, 2, new ProductionPower(res1, res2));
         DevelopmentCard dev2 = new DevelopmentCard(3, res2, CardColor.BLUE, 2, new ProductionPower(res1, res2));
         DevelopmentCard dev3 = new DevelopmentCard(4, res3, CardColor.BLUE, 2, new ProductionPower(res1, res2));
+        DevelopmentCard dev4 = new DevelopmentCard(4, res4, CardColor.BLUE, 2, new ProductionPower(res1, res2));
         deck.addCard(dev1);
         deck.addCard(dev2);
         deck.addCard(dev3);
+        deck.removeCard();
+        deck.removeCard();
+        deck.removeCard();
+        assertNull(deck.getCard());//return null
+        deck.addCard(dev1);
+        deck.addCard(dev2);
+        deck.addCard(dev3);
+        deck.addCard(dev4);
+        deck.removeCard();
         deck.removeCard();
         deck.removeCard();
         deck.removeCard();

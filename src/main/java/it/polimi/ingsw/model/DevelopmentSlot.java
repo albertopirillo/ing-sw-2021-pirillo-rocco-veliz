15a09c742dd984a14bea00abf4993baf8fe95df5
aldairveliz <aldair99veliz@gmail.com>
2021-04-13 12:12:48 +0200

    Added DevelopmentSlot class methods

--- src/main/java/it/polimi/ingsw/model/DevelopmentSlot.java
+++ src/main/java/it/polimi/ingsw/model/DevelopmentSlot.java
@@ -1,15 +1,43 @@
 package it.polimi.ingsw.model;
 
+import it.polimi.ingsw.exceptions.DevSlotEmptyException;
+
 import java.util.*;
 
 public class DevelopmentSlot {
 
-    public DevelopmentSlot() {
-    }
-
-    private DevelopmentCard[] cards;
-
+    private Deque<DevelopmentCard> cards;
     private boolean isProductionActive;
 
+    public DevelopmentSlot() {
+        this.cards = new LinkedList<>();
+    }
 
+    public List<DevelopmentCard> getCards(){
+        return new ArrayList<>(cards);
+    }
+    public void addCard(DevelopmentCard card){
+        cards.push(card);
+    }
+
+    public int numberOfElements(){
+        return cards.size();
+    }
+    public int getLevelSlot() throws DevSlotEmptyException {
+        if(cards.isEmpty()) throw new DevSlotEmptyException();
+        return cards.peek().getLevel();
+    }
+    public DevelopmentCard getTopCard(){
+        return cards.peek();
+    }
+    public boolean isProductionActive() {
+        return isProductionActive;
+    }
+    public void setProductionActive(boolean productionActive) {
+        isProductionActive = productionActive;
+    }
+
+    public boolean canBeAdded(DevelopmentCard card) throws DevSlotEmptyException {
+        return cards.isEmpty() || cards.size()<3 && getLevelSlot() == card.getLevel() + 1;
+    }
 }

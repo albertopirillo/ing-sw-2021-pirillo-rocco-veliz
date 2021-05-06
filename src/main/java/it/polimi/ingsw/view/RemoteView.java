1f33e2f20a9d6a47cc3ad42e79981830c17f32f0
FynePool <riccardomarino.rocco@mail.polimi.it>
2021-05-06 09:46:04 +0200

    Merge master
    Signed-off-by: FynePool <riccardomarino.rocco@mail.polimi.it>

--- src/main/java/it/polimi/ingsw/view/RemoteView.java
+++ src/main/java/it/polimi/ingsw/view/RemoteView.java
@@ -3,10 +3,7 @@ package it.polimi.ingsw.view;
 import it.polimi.ingsw.model.Game;
 import it.polimi.ingsw.model.LeaderCard;
 import it.polimi.ingsw.model.Player;
-import it.polimi.ingsw.network.CardsMessage;
-import it.polimi.ingsw.network.Message;
-import it.polimi.ingsw.network.MessageType;
-import it.polimi.ingsw.network.Request;
+import it.polimi.ingsw.network.*;
 import it.polimi.ingsw.server.Connection;
 import it.polimi.ingsw.server.Server;
 
@@ -20,9 +17,7 @@ public class RemoteView extends View {
     }
 
     public void notifyInitResources(Game game, int numPlayer){
-        Message msg = new Message(numPlayer);
-        msg.setActivePlayer(game.getActivePlayer().getNickname());
-        msg.setType(MessageType.INITIAL_RESOURCE);
+        ServerUpdate msg = new InitialResourcesMessage(game.getActivePlayer().getNickname(), false, numPlayer);
         connection.sendMessage(msg);
     }
 
@@ -30,10 +25,9 @@ public class RemoteView extends View {
         //Prepare message with initial Leader Cards
         Player activePlayer = game.getActivePlayer();
         List<LeaderCard> cards = new ArrayList<>(activePlayer.getLeaderCards());
-        Message msg = new CardsMessage(cards);
-        msg.setType(MessageType.INITIAL_CARDS);
-        msg.setActivePlayer(activePlayer.getNickname());
-        msg.setText("prova");
+        ServerUpdate msg = new LeaderCardsUpdate(activePlayer.getNickname(), false, cards);
+        //msg.setType(MessageType.INITIAL_CARDS);
+        //msg.setText("prova");
         connection.sendMessage(msg);
     }
 
@@ -45,9 +39,8 @@ public class RemoteView extends View {
     public void showGameState(Game game){
         //Preparare messaggio con board compelta da inviare al cliente
         //connection.sendMessage(null);
-        Message msg = new Message("Simulazione turno di gioco");
-        msg.setType(MessageType.PLAYER_MOVE);
-        msg.setActivePlayer(game.getActivePlayer().getNickname());
+        ServerUpdate msg = new TestUpdate(game.getActivePlayer().getNickname(), false, "Simulazione turno di gioco");
+        //msg.setType(MessageType.PLAYER_MOVE);
         connection.sendMessage(msg);
     }
 

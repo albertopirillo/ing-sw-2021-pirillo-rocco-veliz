984b6ef92e9076c89e909183f619e6e291c078ac
AlbertoPirillo <alberto.pirillo@mail.polimi.it>
2021-05-12 19:54:57 +0200

    Added warning before quitting

--- src/main/java/it/polimi/ingsw/client/gui/SceneBuilder.java
+++ src/main/java/it/polimi/ingsw/client/gui/SceneBuilder.java
@@ -4,10 +4,13 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.util.Objects;
+import java.util.Optional;
 
 public class SceneBuilder extends Application {
 
@@ -17,7 +20,7 @@ public class SceneBuilder extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/fxml/scene1.fxml")));
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/fxml/test.fxml")));
         Scene scene = new Scene(root);
         /*String css = Objects.requireNonNull(this.getClass().getResource("/test.css")).toExternalForm();
         scene.getStylesheets().add(css);*/
@@ -25,7 +28,23 @@ public class SceneBuilder extends Application {
         stage.setTitle("Master of Renaissance");
         Image icon = new Image("/png/generic/inkwell.png");
         stage.getIcons().add(icon);
-
+        stage.setOnCloseRequest(event -> {
+            event.consume();
+            quit(stage);
+        });
         stage.show();
+    }
+
+    public void quit(Stage stage) {
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Quit");
+        alert.setHeaderText("You're about to quit");
+        alert.setContentText("All data will be lost");
+        Optional<ButtonType> button = alert.showAndWait();
+        if (button.isPresent()) {
+            if (button.get() == ButtonType.OK) {
+                stage.close();
+            }
+        }
     }
 }

ec02c2e5f56e7b557c56e57b95f1f603d057c241
AlbertoPirillo <alberto.pirillo@mail.polimi.it>
2021-05-23 19:45:54 +0200

    Added run configuration to test a .fxml file without interacting with the server

---
+++ src/main/java/it/polimi/ingsw/client/gui/Testing.java
@@ -0,0 +1,31 @@
+package it.polimi.ingsw.client.gui;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Testing extends Application {
+
+    public static void main(String[] args) {
+        launch();
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        //Load FXML and set the scene
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/setup.fxml"));
+        Parent root = loader.load();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.setResizable(false);
+
+        //Load CSS
+        String css = Util.getCSS("main");
+        scene.getStylesheets().add(css);
+
+        stage.centerOnScreen();
+        stage.show();
+    }
+}

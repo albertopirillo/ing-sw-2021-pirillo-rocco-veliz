771ebb5868d149dd62ca0a11a612764fdf45a707
aldairveliz <aldair99veliz@gmail.com>
2021-05-28 13:18:41 +0200

    Add JavaDoc

--- src/main/java/it/polimi/ingsw/client/gui/TrayController.java
+++ src/main/java/it/polimi/ingsw/client/gui/TrayController.java
@@ -18,14 +18,31 @@ public class TrayController implements Initializable {
     @FXML
     private ImageView tray_1, tray_2, tray_3, tray_4, tray_5, tray_6, tray_7, tray_8, tray_9, tray_10, tray_11, tray_12, tray_white;
 
+    /**
+     * The list of all twelve corresponding images of the Market Matrix
+     */
     private final List<ImageView> marketTray = new ArrayList<>();
+    /**
+     * The location of the remaining marble and where the current drag started
+     */
     private ImageView remaining;
+    /**
+     * The corresponding MainController
+     */
     private MainController mainController;
 
+    /**
+     * Sets the MainController
+     * @param mainController  the MainController to associate with this controller
+     */
     public void setMainController(MainController mainController) {
         this.mainController = mainController;
     }
 
+    /**
+     * Initializes @FXML fields, organizing the corresponding images of Market Matrix in one list<br>
+     * <p>Called automatically when an entity is injected from FXML</p>
+     */
     public void initialize(URL url, ResourceBundle resourceBundle) {
         this.marketTray.add(tray_1);
         this.marketTray.add(tray_2);
@@ -42,6 +59,11 @@ public class TrayController implements Initializable {
         this.remaining = tray_white;
     }
 
+    /**
+     * Update the Market Tray
+     * @param marbles contains the twelve marbles that compose the Market matrix
+     * @param remainingMarble the remaining marble
+     */
     public void updateMarketTray(MarblesColor[][] marbles, MarblesColor remainingMarble) {
         for(int i=0; i<3; i++){
             for(int j=0; j<4; j++){
@@ -61,6 +83,10 @@ public class TrayController implements Initializable {
         event.consume();
     }
 
+    /**
+     * Send an InsertMarbleRequest to the server
+     * @param event The drop event
+     */
     public void dragDrop(DragEvent event) {
         System.out.println("DRAG DROPPED");
         ImageView destination = (ImageView) event.getSource();

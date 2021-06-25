215713ee091ddad155de3e7bd81642daff22416f
FynePool <riccardomarino.rocco@mail.polimi.it>
2021-06-25 13:03:00 +0200

    Fixing Basic Production Warning
    Signed-off-by: FynePool <riccardomarino.rocco@mail.polimi.it>

--- src/main/java/it/polimi/ingsw/client/gui/BasicProductionController.java
+++ src/main/java/it/polimi/ingsw/client/gui/BasicProductionController.java
@@ -8,9 +8,7 @@ import it.polimi.ingsw.network.requests.Request;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
-import javafx.scene.control.RadioButton;
 import javafx.scene.control.ToggleGroup;
 
 import java.net.URL;
@@ -28,10 +26,6 @@ public class BasicProductionController implements Initializable {
 
     @FXML
     private ChoiceBox choiceBox1, choiceBox2;
-    @FXML
-    private RadioButton rdIN1_1, rdIN1_2, rdIN1_3, rdIN1_4, rdIN2_1, rdIN2_2, rdIN2_3, rdIN2_4, rdOUT_1, rdOUT_2, rdOUT_3, rdOUT_4;
-    @FXML
-    private Button btnConfirm;
     @FXML
     private ToggleGroup toggleGroupIN1, toggleGroupIN2, toggleGroupOUT;
 
@@ -56,18 +50,18 @@ public class BasicProductionController implements Initializable {
     /**
      * Based on selection sets input1, input2 and output ResourceType
      */
-    public void onClickStone1(ActionEvent actionEvent) { this.input1 = ResourceType.STONE; }
-    public void onClickCoin1(ActionEvent actionEvent) { this.input1 = ResourceType.COIN; }
-    public void onClickShield1(ActionEvent actionEvent) { this.input1 = ResourceType.SHIELD; }
-    public void onClickServant1(ActionEvent actionEvent) { this.input1 = ResourceType.SERVANT; }
-    public void onClickStone2(ActionEvent actionEvent) { this.input2 = ResourceType.STONE; }
-    public void onClickCoin2(ActionEvent actionEvent) { this.input2 = ResourceType.COIN; }
-    public void onClickShield2(ActionEvent actionEvent) { this.input2 = ResourceType.SHIELD; }
-    public void onClickServant2(ActionEvent actionEvent) { this.input2 = ResourceType.SERVANT; }
-    public void onClickStone3(ActionEvent actionEvent) { this.output = ResourceType.STONE; }
-    public void onClickCoin3(ActionEvent actionEvent) { this.output = ResourceType.COIN; }
-    public void onClickShield3(ActionEvent actionEvent) { this.output = ResourceType.SHIELD; }
-    public void onClickServant3(ActionEvent actionEvent) { this.output = ResourceType.SERVANT; }
+    public void onClickStone1() { this.input1 = ResourceType.STONE; }
+    public void onClickCoin1() { this.input1 = ResourceType.COIN; }
+    public void onClickShield1() { this.input1 = ResourceType.SHIELD; }
+    public void onClickServant1() { this.input1 = ResourceType.SERVANT; }
+    public void onClickStone2() { this.input2 = ResourceType.STONE; }
+    public void onClickCoin2() { this.input2 = ResourceType.COIN; }
+    public void onClickShield2() { this.input2 = ResourceType.SHIELD; }
+    public void onClickServant2() { this.input2 = ResourceType.SERVANT; }
+    public void onClickStone3() { this.output = ResourceType.STONE; }
+    public void onClickCoin3() { this.output = ResourceType.COIN; }
+    public void onClickShield3() { this.output = ResourceType.SHIELD; }
+    public void onClickServant3() { this.output = ResourceType.SERVANT; }
 
 
     /**
